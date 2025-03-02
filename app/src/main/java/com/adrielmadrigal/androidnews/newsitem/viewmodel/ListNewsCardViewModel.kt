583c6d9909df6e1a9f9c6436631694d19b6b80ee
Adriel Madrigal <itsadrielm@gmail.com>
2025-03-02 01:33:47 -0800

    NEWS-APP: Removed unused code and comments

--- app/src/main/java/com/adrielmadrigal/androidnews/newsitem/viewmodel/ListNewsCardViewModel.kt
+++ app/src/main/java/com/adrielmadrigal/androidnews/newsitem/viewmodel/ListNewsCardViewModel.kt
@@ -1,21 +1,15 @@
 package com.adrielmadrigal.androidnews.newsitem.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.adrielmadrigal.androidnews.newsapi.data.model.NewsModel
-import com.adrielmadrigal.androidnews.newsapi.services.NewsResult
 import com.adrielmadrigal.androidnews.newsitem.usecase.FetchNewsArticlesUseCase
 import com.adrielmadrigal.androidnews.newsitem.view.ListNewsCardUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,9 +21,6 @@ class ListNewsCardViewModel @Inject constructor(
     private val mutableUiState = MutableStateFlow<ListNewsCardUiState>(ListNewsCardUiState.Idle)
     val uiState = mutableUiState.asStateFlow()
 
-//    private val _newsResult = MutableLiveData<NewsResult>(NewsResult.Loading)
-//    val newsResult: LiveData<NewsResult>
-//        get() = _newsResult
 
     init {
         fetchNews()
@@ -37,18 +28,6 @@ class ListNewsCardViewModel @Inject constructor(
 
     private fun fetchNews() {
 
-//        viewModelScope.launch {
-//            mutableUiState.update {
-//                ListNewsCardUiState.Loading
-//            }
-//            delay(500)
-//            val news = fetchNewsArticlesUseCase()
-//            mutableUiState.update {
-//                ListNewsCardUiState.Success(
-//
-//                )
-//            }
-//        }
         mutableUiState.update {
             ListNewsCardUiState.Loading
         }
